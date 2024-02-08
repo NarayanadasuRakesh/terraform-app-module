f2dabdf2e23582bd7b27d92e727759963559117e
NarayanadasuRakesh <narayanadasurakesh@gmail.com>
2024-02-08 22:52:43 +0530

    terraform module for app components

---
+++ variables.tf
@@ -0,0 +1,49 @@
+variable "common_tags" {
+  type = map
+}
+
+variable "project_name" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "tags" {
+  type = map
+}
+
+variable "vpc_id" {
+  
+}
+
+variable "component_sg_id" {
+  
+}
+
+variable "zone_name" {
+  type = string
+}
+
+
+variable "private_subnet_ids" {
+  
+}
+
+variable "iam_instance_profile" {
+  type = string
+}
+
+variable "app_alb_listener_arn" {
+  
+}
+
+variable "rule_priority" {
+  
+}
+
+variable "app_version" {
+  
+}
+
